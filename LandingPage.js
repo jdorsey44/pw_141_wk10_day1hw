12d493d95c80600f9fa773d628d3679936c2e476
jdorsey44 <159090126+jdorsey44@users.noreply.github.com>
2024-04-17 11:54:43 -0700

    Add files via upload

---
+++ LandingPage.js
@@ -0,0 +1,12 @@
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to My React App</h1>
+      <p>This is the landing page of my React app.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ NavBar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2>My React App</h2>
+      {/* Add your navigation links here */}
+    </nav>
+  );
+}
+
+export default Navbar;
